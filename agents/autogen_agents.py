26db7e11d3cb06906ba44340f771154d8e57c054
KHMSmartBuild <kylemorgan9@gmail.com>
2023-10-24 15:34:46 +0100

    Added scripts, seed data, and re-organized project structure

--- agents/autogen_agents.py
+++ agents/autogen_agents.py
@@ -1,10 +1,18 @@
 import json
 import os
-import autogen as ag
 from agents.config import config_list_gpt4
 from eco_buddies import Eco_Bot
 from icecream import ic
+from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
 
+# Load LLM inference endpoints from an env variable or a file
+# See https://microsoft.github.io/autogen/docs/FAQ#set-your-api-endpoints
+# and OAI_CONFIG_LIST_sample.json
+config_list = config_list_from_json(env_or_file="config_list_gpt4")
+assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
+user_proxy = UserProxyAgent("user_proxy", code_execution_config={"work_dir": "coding"})
+user_proxy.initiate_chat(assistant, message=("..."))
+# This initiates an automated chat between the two agents to solve the task
 ic = ic.configureOutput(prefix="")
 Eco_Bot = Eco_Bot()
 class GeneralManagerAgent:
